a439e90d9fdfe13f58c957d5087184f15bf71cb7
Bala <balakumaris015@gmail.com>
2026-05-16 10:19:53 +0530

    Update TestController.java

--- JenkinsTest/src/main/java/com/bala/JenkinsTest/Controller/TestController.java
+++ JenkinsTest/src/main/java/com/bala/JenkinsTest/Controller/TestController.java
@@ -8,7 +8,6 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello Bala Kumar S, You are doing great!!! Good morning, Now you are able to get the result directly from the GIT"+ new String(Character.toChars(0x1F349)) +"
-             Poll SCM added";
+        return "Hello Bala Kumar S, You are doing great!!! Good morning, Now you are able to get the result directly from the GIT"+ new String(Character.toChars(0x1F349)) +" Poll SCM added";
     }
 }
