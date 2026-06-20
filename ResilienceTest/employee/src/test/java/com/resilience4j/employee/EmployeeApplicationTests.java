2f25b21cc53e71c64124722b3515a2b1505d329c
Bala <balakumaris015>
2026-06-20 11:47:37 +0530

    Initial commit of Employee SB app

---
+++ ResilienceTest/employee/src/test/java/com/resilience4j/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resilience4j.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
